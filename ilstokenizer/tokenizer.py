6ce3bc28f75dfdd257400decb9ded6e2a4a5dab2
vandan <vmujadia@gmail.com>
2020-01-19 11:02:10 +0530

    options to get lower

--- ilstokenizer/tokenizer.py
+++ ilstokenizer/tokenizer.py
@@ -1,9 +1,13 @@
 import ilstokenizer._tokenizer_ils as _tokenizer_ils
 
-def tokenize(sentence):
+def tokenize(sentence, to_lower=False):
+    if to_lower:
+        sentence = sentence.lower()
     sentence = sentence.split()
     return " ".join(_tokenizer_ils.tokenize(sentence))
 
-def tokenize_text(text):
+def tokenize_text(text, to_lower=False):
+    if to_lower:
+        text = text.lower()
     text = text.split()
     return " ".join(_tokenizer_ils.tokenize_text(text))
